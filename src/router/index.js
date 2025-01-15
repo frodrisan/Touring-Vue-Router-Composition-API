71edcf05c5b6d87453eaf3a1590e240b77770877
Fabián RS <frodriguez@fulp.es>
2025-01-15 10:40:15 +0000

    End 'Wrapping Up' lesson.

--- src/router/index.js
+++ src/router/index.js
@@ -76,6 +76,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) { /* Function to return top page when paging. */
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0}
+    }
+  }
 });
 
 export default router;
